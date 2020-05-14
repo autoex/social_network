9ea203b1105bc04fb872e5aeb2c9a8cd9200974f
autoex <autoex@gmail.com>
2020-05-14 12:28:56 +0300

    add ref, Virtual DOM

--- src/components/Profile/My Posts/MyPosts.jsx
+++ src/components/Profile/My Posts/MyPosts.jsx
@@ -4,21 +4,27 @@ import Post from "./Post/Post"
 
 class MyPosts extends React.Component {
     render() {
-    let posts = this.props.posts;
+        let posts = this.props.posts;
 
 
-     let postsElements = posts.map(p => <Post message={p.message} likes={p.likesCount} />);
+        let postsElements = posts.map(p => <Post message={p.message} likes={p.likesCount}/>);
+        let newPostElement = React.createRef();
+        let addPost = () => {
+            let text = newPostElement.current.value;
+            alert(text);
+        }
+
         return (
             <div className={classes.myPosts}>
                 <div>My posts</div>
                 <div>
                     <div>
-                        <textarea>
+                        <textarea ref={newPostElement}>
                             Some text here...
                         </textarea>
                     </div>
 
-                    <button>Add Post</button>
+                    <button className="btn" onClick={addPost}>Add Post</button>
                 </div>
                 {postsElements}
             </div>
