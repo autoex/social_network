b8c19cf7c9a486f878f5b1bacc07e38c41fd6be1
autoex <autoex@gmail.com>
2020-08-07 12:47:10 +0300

    56 - React JS #2 - remove UserAPIComponent

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,7 +1,50 @@
-import UsersAPIComponent from "./UsersAPIComponent";
 import {connect} from "react-redux";
 import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unFollowAC} from "../../redux/users-reducer";
+import React from "react";
+import * as axios from "axios";
+import Users from "./Users";
 
+
+class UsersComponent extends React.Component {
+
+    componentDidMount() {
+
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(
+                response => {
+                    this.props.setUsers(response.data.items);
+                    this.props.setTotalUsersCount(response.data.totalCount);
+                });
+    };
+
+    onPageChange = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(
+                response => {
+                    this.props.setUsers(response.data.items)
+                });
+
+    };
+
+
+    render() {
+
+
+        return <Users totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      users={this.props.users}
+                      onPageChange={this.onPageChange}
+                      follow={this.props.follow}
+                      unFollow={this.props.unFollow}/>
+
+
+    }
+
+
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -32,5 +75,5 @@ let mapDispatchToProps = (dispatch) => {
 };
 
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent);
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersComponent);
 export default UsersContainer;
