3034781128543466b73d9a9aec48f6673d911d73
autoex <autoex@gmail.com>
2020-10-14 11:29:50 +0300

    75 - React JS - redux-form introduction (login)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,42 +1,51 @@
 import React from "react";
+import {Field, reduxForm} from "redux-form";
 
 
 let Login = (props) => {
-
+    const onSubmit = (formData)=> {
+        console.log(formData)
+    };
 
     return (
         <div>
             <h1>Log in</h1>
-            <LoginForm />
+            <LoginReduxForm onSubmit={onSubmit}/>
 
         </div>
     )
 };
 
-let LoginForm =(props)=> {
+let LoginForm = (props) => {
 
 
     return (
-        <form>
+        <form onSubmit={props.handleSubmit}>
             <p>
-                <input type={"text"} name="" id="" placeholder={'Login'}/>
+                <Field component={"input"} name={"login"} placeholder={'Login'}/>
             </p>
             <p>
-                <input type={"text"} name="" id="" placeholder={'Password'}/>
+                <Field component={"input"} name={"password"} placeholder={'Password'}/>
             </p>
             <p>
-                <input type={"checkbox"} name="" id="rememberMe"/>
+                <Field component={"input"} type={"checkbox"} name={"rememberMe"} id={"rememberMe"}/>
                 <label htmlFor="rememberMe">remember me</label>
             </p>
             <p>
-                <button >Login</button>
+                <button>Login</button>
             </p>
 
         </form>
     )
 
 
-
 };
+
+
+const LoginReduxForm = reduxForm({
+    // a unique name for the form
+    form: 'login'
+})(LoginForm);
+
 
 export default Login;
