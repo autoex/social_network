b2d18141a25d031e59581f193a412459e3e31d0e
autoex <autoex@gmail.com>
2020-10-16 14:39:36 +0300

    77 - React JS - redux-form field validation ALL DONE

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -1,21 +1,27 @@
 import React from "react";
 import classes from "./FormsControls.module.css"
 
-export const Textarea = ({input, meta, ...props}) => {
-const hasError = meta.error && meta.touched;
+
+const FormControl = ({input, meta, child, ...props}) => {
+    const hasError = meta.error && meta.touched;
     return (<div className={classes.formControl+' '+ (hasError && classes.error)} >
-            <textarea {...input} {...props} />
-           {hasError && <span>{meta.error}</span>}
+            {props.children}
+            {hasError && <span>{meta.error}</span>}
         </div>
     )
 };
 
-export const Input = ({input, meta, ...props}) => {
-    const hasError = meta.error && meta.touched;
-    return (<div className={classes.formControl+' '+ (hasError && classes.error)} >
-            <input{...input} {...props} />
-            {hasError && <span>{meta.error}</span>}
-        </div>
-    )
+export const Textarea = (props) => {
+const {input, meta, child, ...restProps} = props;
+return <FormControl {...props}>
+    <textarea {...input} {...restProps}/>
+</FormControl>;
+};
+
+export const Input = (props) => {
+    const {input, meta, child, ...restProps} = props;
+    return <FormControl {...props}>
+        <input {...input} {...restProps}/>
+    </FormControl>;
 };
 
