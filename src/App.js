cdaed39d0b9a96adfe17c360335c09addea43ca8
autoex <autoex@gmail.com>
2020-11-04 10:52:16 +0200

    Add 404 page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./Style.css";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
 import Music from "./components/Music/Music";
-import {Route, withRouter} from "react-router-dom";
+import {Route, Switch, withRouter} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import NavContainer from "./components/Nav/NavContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
+import Error from "./components/Error/Error"
 import {connect} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
@@ -30,22 +31,24 @@ class App extends Component {
                 <HeaderContainer/>
                 <NavContainer/>
                 <div className="app-wrapper-content">
-                    <Route path='/dialogs'
-                           render={() => <DialogsContainer/>}/> {/*Variant 1*/}
-                    {/*<Route path='/profile' render={() => <Profile state={profilePage}/>}/>*/}
+                    <Switch>
+                        <Route path='/dialogs'
+                               render={() => <DialogsContainer/>}/> {/*Variant 1*/}
+                        {/*<Route path='/profile' render={() => <Profile state={profilePage}/>}/>*/}
 
 
-                    <Route path='/profile/:userId?'> <ProfileContainer/></Route> {/*Variant 2*/}
+                        <Route path='/profile/:userId?'> <ProfileContainer/></Route> {/*Variant 2*/}
 
 
-                    <Route path='/users'
-                           render={() => <UsersContainer/>}/>
+                        <Route path='/users'
+                               render={() => <UsersContainer/>}/>
 
-                    <Route path='/news' component={News}/> {/*Variant 3*/}
-                    <Route path='/settings' component={Settings}/>
-                    <Route path='/music' component={Music}/>
-                    <Route path='/login' component={Login}/>
-
+                        <Route path='/news' component={News}/> {/*Variant 3*/}
+                        <Route path='/settings' component={Settings}/>
+                        <Route path='/music' component={Music}/>
+                        <Route path='/login' component={Login}/>
+                        <Route component={Error}/>
+                    </Switch>
 
                 </div>
                 {/*  <Footer /> */}
