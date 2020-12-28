3edab0120fea8d00150e708b7f00894ad53d2d7a
autoex <autoex@gmail.com>
2020-12-28 20:26:35 +0200

    some work with paginator

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -3,8 +3,8 @@ import classes from "./Paginator.module.css"
 
 let Paginator = ({totalUsersCount, pageSize, currentPage, selectedPage, onPageChange}) => {
 
-    // let pagesCount = Math.ceil(totalUsersCount / pageSize);
-    let pagesCount = 30;
+    let pagesCount = Math.ceil(totalUsersCount / pageSize);
+    // let pagesCount = 30;
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
