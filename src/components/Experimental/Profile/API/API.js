38eac008eb45fdf1f2bae021907c536f1da7b787
autoex <autoex@gmail.com>
2021-06-03 12:41:16 +0300

    replace json-server from global to local

--- src/components/Experimental/Profile/API/API.js
+++ src/components/Experimental/Profile/API/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const localInstance = axios.create({
-    baseURL: 'http://localhost:3000/users/',
+    baseURL: 'http://localhost:5000/users/',
     headers: {'Content-Type': 'application/json'}
 });
 
